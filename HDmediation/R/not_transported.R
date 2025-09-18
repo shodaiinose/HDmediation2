0ab2237bd6ef1148a0c8810c27f005b5532ae66d
Shodai <68819024+shodaiinose@users.noreply.github.com>
2025-09-18 13:01:07 -0400

    Update not_transported.R

--- HDmediation/R/not_transported.R
+++ HDmediation/R/not_transported.R
@@ -45,9 +45,12 @@ not_transported <- function(data, A, W, Z, M, Y, cens,
         }
 
         ipwy <- ((A == aprime) / gg[, gl("g({aprime}|w)")])*ipcw_ap
+        density <- ipwy * hm / mean(ipwy * hm) # return this
+        density_trimmed <- pmin(density, quantile(density, 0.999)) # trim to the 99th percentile
+      
         if (partial_tmle) {
             fit <- glm(Y ~ 1, offset = qlogis(bb[, gl("b({aprime},Z,M,W)")]), family = "binomial",
-                       subset = A == aprime, weights = ipwy * hm / mean(ipwy * hm))
+                       subset = A == aprime, weights = density_trimmed)
             bb[, gl("b({aprime},Z,M,W)")] <- plogis(coef(fit) + qlogis(bb[, gl("b({aprime},Z,M,W)")]))
         }
 
@@ -57,7 +60,7 @@ not_transported <- function(data, A, W, Z, M, Y, cens,
         vvbar[, paste(param, collapse = "")] <- vbar(data, npsem, vv, astar, folds, learners_vbar)
 
         # EIF calculation
-        eify <- ipwy * hm / mean(ipwy * hm) * (Y - bb[, gl("b({aprime},Z,M,W)")])
+        eify <- density_trimmed * (Y - bb[, gl("b({aprime},Z,M,W)")])
         # eify <- ipwy * hm * (Y - bb[, gl("b({aprime},Z,M,W)")])
 
         ipwz <- ((A == aprime) / gg[, gl("g({aprime}|w)")])*ipcw_ap
